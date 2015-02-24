296c5b7c5940cef497fb2e30a9b083f00e2b74e6
duwonders <752046978@qq.com>
2015-02-24 20:49:00 +0800

    fix bug

--- home/photo.php
+++ home/photo.php
@@ -1,15 +1,18 @@
 <meta charset="UTF-8">
 <?php
 include("../admin/conn.php");
-echo $_FILES['myfile']['name'];
 $picture = 'image/'.$_FILES['myfile']['name'];
 move_uploaded_file($_FILES['myfile']['tmp_name'] , $picture);
-echo $picture;  
 @$username = $_COOKIE[username];
+echo"$username";
+echo"$picture";
 $sql = "insert into `photo`(username,iamge) 
 values('$username','$picture')";
 $res = $dbh->prepare($sql);
 $res->execute();
-echo"<script language=\"javascript\">
-	location.href='photo.php';</script>";
+    echo'<script language = "javascript">';
+    echo'alert("上传成功")';
+	echo'</script>';
+    echo"<script language=\"javascript\">
+	location.href='album.php';</script>";
 ?>
